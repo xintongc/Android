6e8c60670dfb05819a90b58489a35c67c78c7852
Xintong Cheng <xintongc@gmail.com>
2018-10-07 23:34:43 -0400

    Add files via upload

---
+++ RecyclerView/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.zncu.listviewtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+//    private String[] data = {"Apple","Banana","Orange","watermelon","Pear","Grape","Pineapple","Strawberry","Cherry",
+//            "Mango","Apple","Banana","Orange","Watermelon","Pear","Grape","Pineapple","Strawberry","Cherry","Mango"};
+
+
+    private List<Fruit> fruitList = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+        FruitAdapter adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
+    private void initFruits(){
+        for(int i = 0; i < 10; i++){
+            Fruit apple = new Fruit("Apple",R.drawable.apple_pic);
+            fruitList.add(apple);
+            Fruit banana = new Fruit("Banana",R.drawable.banana_pic);
+            fruitList.add(banana);
+        }
+    }
+}
